da58efb5f2c458ada76e68ba749dff2bd97e84f0
Ridhima Bhatt <131640173+iamridhima@users.noreply.github.com>
2024-02-02 22:10:37 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,77 +1,77 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// const App = () => {
-//   const [count, setCount] = useState(0);
+const App = () => {
+  const [count, setCount] = useState(0);
 
-//   const Plus = (num) => {
-//     if(num==-2 && count<=1){
-//       return
-//     }
-//     setCount(count + num)
-//   }
+  const Plus = (num) => {
+    if(num==-2 && count<=1){
+      return
+    }
+    setCount(count + num)
+  }
 
-//   return (
-//     <>
-//     Count : {count}
-//       <button
-//         onClick={()=>Plus(1)}
-//       >+1</button>
-//       <button
-//         onClick={()=>Plus(-2)}
-//       >-2</button>
-//     </>
+  return (
+    <>
+    Count : {count}
+      <button
+        onClick={()=>Plus(1)}
+      >+1</button>
+      <button
+        onClick={()=>Plus(-2)}
+      >-2</button>
+    </>
 
-//   );
-// }
-//   export default App;
-// import React, { useState } from 'react';
+  );
+}
+  export default App;
+import React, { useState } from 'react';
 
-// const NameList = () => {
-//   const [names, setNames] = useState([]);
-//   const [newName, setNewName] = useState({ firstname: "", Age: "" });
+const NameList = () => {
+  const [names, setNames] = useState([]);
+  const [newName, setNewName] = useState({ firstname: "", Age: "" });
 
-//   const handleNameChange = (event) => {
-//     setNewName({ ...newName, firstname: event.target.value });
-//   };
-//   const handleAgeChange = (event) => {
-//     setNewName({ ...newName, Age: event.target.value });
-//   };
+  const handleNameChange = (event) => {
+    setNewName({ ...newName, firstname: event.target.value });
+  };
+  const handleAgeChange = (event) => {
+    setNewName({ ...newName, Age: event.target.value });
+  };
 
-//   const handleAddName = (e) => {
-//     e.preventDefault(e);
-//     if (newName.firstname ==="" ||newName.Age==="") {
-//       return
-//     }
-//     setNames([...names, newName]);
-//     setNewName({firstname:"",Age:""});
+  const handleAddName = (e) => {
+    e.preventDefault(e);
+    if (newName.firstname ==="" ||newName.Age==="") {
+      return
+    }
+    setNames([...names, newName]);
+    setNewName({firstname:"",Age:""});
 
-//   };
+  };
 
-//   return (
-//     <div>
-//       <div>
-//         <input
-//           type="text"
-//           value={newName.firstname}
-//           onChange={handleNameChange}
-//         />
-//         <input
-//           type="number"
-//           value={newName.Age}
-//           onChange={handleAgeChange}
-//         />
-//         <button onClick={handleAddName}>Add</button>
-//       </div>
-//       <ul>
-//         {names.map((name, index) => (
-//           <li key={index}>{name.firstname},{name.Age}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={newName.firstname}
+          onChange={handleNameChange}
+        />
+        <input
+          type="number"
+          value={newName.Age}
+          onChange={handleAgeChange}
+        />
+        <button onClick={handleAddName}>Add</button>
+      </div>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name.firstname},{name.Age}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-// export default NameList;
+export default NameList;
 
 import React, { useState } from 'react';
 
